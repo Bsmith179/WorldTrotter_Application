de79b218dac66b9e0bff34a55dea1cfc0506dfe0
Brigitte Smith <Bsmith179@student.cscc.edu>
2026-02-26 05:12:47 -0500

    Updated headder comments for Unit 5 assignment.

--- WorldTrotter/MapViewController.swift
+++ WorldTrotter/MapViewController.swift
@@ -2,8 +2,8 @@
 //  MapViewController.swift
 //  WorldTrotter
 //  An app that runs region specific temperature conversions selectable by regions on a map.
-//  Created by Icy on 2/21/26.
-//  Updated on 2/21/26
+//  Created by Brigitte on 2/21/26.
+//  Updated on 2/26/26 - Refactored the MapViewController to load its view programmatically.
 //
 
 import UIKit
