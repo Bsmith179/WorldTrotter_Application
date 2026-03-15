314dca92871f7796016ddc1140eb877e92b2ab00
Brigitte Smith <Bsmith179@student.cscc.edu>
2026-03-15 12:04:10 -0400

    Added spanish translations for map type titles.

--- WorldTrotter/MapViewController.swift
+++ WorldTrotter/MapViewController.swift
@@ -25,8 +25,13 @@ class MapViewController: UIViewController {
         // Set Initial PoI Visibility
         mapView.pointOfInterestFilter = .includingAll
         
+        let standardString = NSLocalizedString("Standard", comment: "Standard map view")
+        let hybridString = NSLocalizedString("Hybrid", comment: "Hybrid map view")
+        let satelliteString
+                = NSLocalizedString("Satellite", comment: "Satellite map view")
+
         let segmentedControl
-        = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
+              = UISegmentedControl(items: [standardString, hybridString, satelliteString])
         segmentedControl.backgroundColor = UIColor.systemBackground
         segmentedControl.selectedSegmentIndex = 0
         segmentedControl.translatesAutoresizingMaskIntoConstraints = false
