7810e522e3fdd4832e0850cf7f960c9bc2544abd
Brigitte Smith <Bsmith179@student.cscc.edu>
2026-03-15 03:35:48 -0400

    Implemented functional and mathmatically conversion from F to C. Added delegation for the UITextField to prevent the use of multiple decimals pointsin a number.

--- WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/ConversionViewController.swift
@@ -9,16 +9,34 @@
 
 import UIKit
 
-class ConversionViewController: UIViewController {
-
+class ConversionViewController: UIViewController, UITextFieldDelegate {
+    
     @IBOutlet var celsiusLabel: UILabel!
+    @IBOutlet var textField: UITextField!
+    
+    var fahrenheitValue: Measurement<UnitTemperature>?{
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
+    
+    var celsiusValue: Measurement<UnitTemperature>? {
+        if let fahrenheitValue = fahrenheitValue {
+            return fahrenheitValue.converted(to: .celsius)
+        } else {
+            return nil
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         print("ConversionViewController loaded its view.")
-
-
+        
+        textField.delegate = self
+        
+        updateCelsiusLabel()
+        
         // let firstFrame = CGRect(x: 160, y: 240, width: 100, height: 150)
         // let firstView = UIView(frame: firstFrame)
         // firstView.backgroundColor = UIColor.blue
@@ -29,15 +47,51 @@ class ConversionViewController: UIViewController {
         // secondView.backgroundColor = UIColor.green
         // firstView.addSubview(secondView)
     }
-
+    
     @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
         
-        if let text = textField.text, !text.isEmpty {
-            celsiusLabel.text = text
+        if let text = textField.text, let value = Double(text) {
+            fahrenheitValue = Measurement(value: value, unit: .fahrenheit)
+        } else {
+            fahrenheitValue = nil
+        }
+    }
+    
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+        textField.resignFirstResponder()
+    }
+    
+    
+    let numberFormatter: NumberFormatter = {
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+    }()
+    
+    func updateCelsiusLabel() {
+        if let celsiusValue = celsiusValue {
+            celsiusLabel.text =
+            numberFormatter.string(from: NSNumber(value: celsiusValue.value))
         } else {
             celsiusLabel.text = "???"
         }
     }
-
+    
+    func textField(_ textField: UITextField,
+                   shouldChangeCharactersIn range: NSRange,
+                   replacementString string: String) -> Bool {
+        
+        let existingTextHasDecimalSeparator = textField.text?.range(of: ".")
+        let replacementTextHasDecimalSeparator = string.range(of: ".")
+        
+        if existingTextHasDecimalSeparator != nil,
+           replacementTextHasDecimalSeparator != nil {
+            return false
+        } else {
+            return true
+        }
+    }
+    
 }
-
